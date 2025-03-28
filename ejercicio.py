d0ad4a8425f9d30370d8675b215a9ad87223d661
JavierGuzman9844 <javieralejandroguzmanpadillla@gmail.com>
2025-03-28 17:29:25 -0600

    Create ejercicio.py
    hi my second project.

---
+++ ejercicio.py
@@ -0,0 +1,17 @@
+while True:
+    meme_dict = {
+                "CRINGE": "Algo excepcionalmente raro o embarazoso",
+                "LOL": "Una respuesta común a algo gracioso",
+                "XD": "Una forma de poner que algo dio risa",
+                "ROFL": "una respuesta a una broma",
+                "SHEESH": "ligera desaprobación",
+                "CREEPY": "aterrador, siniestro",
+                "AGGRO" : "ponerse agresivo/enojado",
+                }
+
+    word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print("Esa palabra no se encuentra en mi vocabulario")
